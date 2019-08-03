4d6e2e0224b0047101db13afe673ac7b67723b91
renaud.henry <renaud.henry@live.fr>
2019-08-03 09:57:36 +0100

    Versionning Programme + Update Programe + Ajout de jeux de données

---
+++ src/Programme_V1.1.0.0/WpfApp_images/Lib/Module/Module_Signals.vb
@@ -0,0 +1,34 @@
+﻿Public Module Module_Signals
+    Private Function SignalCarre(pos As Single, len As Single) As Single
+        Dim Position = Math.Abs(pos / len)
+        If Position <= CSng(0.75) Then
+            Return CSng(2.0)
+        Else
+            Return CSng(0.0)
+        End If
+    End Function
+
+    Private Function SignalTriangle(pos As Single, len As Single) As Single
+        Dim Position = 1 - Math.Abs(pos / len)
+        Return Position * CSng(2.0)
+    End Function
+
+
+    Private Function SignalSinus(pos As Single, len As Single) As Single
+        Dim Position = (1 - Math.Abs(CDbl(pos / len))) * (Math.PI / 2.0)
+        Return CSng(Math.Sin(Position) * 2.0)
+    End Function
+
+    Public Function Signal(pos As Single, len As Single, window As MainWindow) As Single
+        If window.RadioButton_Transition_Carré.IsChecked Then
+            Return SignalCarre(pos, len)
+        ElseIf window.RadioButton_Transition_Triangle.IsChecked Then
+            Return SignalTriangle(pos, len)
+        ElseIf window.RadioButton_Transition_Sinus.IsChecked Then
+            Return SignalSinus(pos, len)
+        Else
+            Return CSng(0.0)
+        End If
+    End Function
+
+End Module
